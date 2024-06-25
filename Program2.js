55ff2fee286a049a540a9e1dba5399b1c06b5049
haritha <haritha7319@gmail.com>
2024-06-25 10:26:13 +0530

    Login page Program version1

---
+++ Program2.js
@@ -0,0 +1,24 @@
+
+let storedUserName = "Arun123";
+let storedPassword =  "Arun@23"
+
+let inputUserName = "Arun13";
+let inputPassword  = "Arun@23";
+
+
+
+if(storedUserName == inputUserName)
+{
+  if(storedPassword == inputPassword)
+  {
+    console.log(" Successfully logged in");
+  }
+  else
+  {
+    console.log(" Check the  Password");
+  }
+}
+else
+{
+  console.log(" check the UserName");
+}
